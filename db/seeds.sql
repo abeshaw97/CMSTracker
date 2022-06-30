12634f4e3ee8558ba93a97df0cea7711b9ec27c7
abeshaw97 <shawabra@msu.edu>
2022-06-29 22:24:49 -0400

    added seed with notes and scehma files

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS CMStrackerdb;
+CREATE DATABASE CMStrackerdb;
+
+USE CMStrackerdb;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id),
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+     PRIMARY KEY(id),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
